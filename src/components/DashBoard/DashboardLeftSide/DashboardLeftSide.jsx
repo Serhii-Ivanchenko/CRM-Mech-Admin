a8dedf8741b53c7cc013e9d35b08ed1db9aaa6c7
Juliiaks <juliakharetonchuk@gmail.com>
2025-02-01 19:42:02 +0200

    finished left side

--- src/components/DashBoard/DashboardLeftSide/DashboardLeftSide.jsx
+++ src/components/DashBoard/DashboardLeftSide/DashboardLeftSide.jsx
@@ -1,5 +1,13 @@
 import css from "./DashboardLeftSide.module.css";
 import { BsPersonFill } from "react-icons/bs";
+import { BsClipboardData } from "react-icons/bs";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { BsBriefcaseFill } from "react-icons/bs";
+import gmail from "../../../assets/images/IconsDashboardLeftSide/gmail.png";
+import meta from "../../../assets/images/IconsDashboardLeftSide/Meta.png";
+import partners from "../../../assets/images/IconsDashboardLeftSide/Partners 1.png";
+import profit from "../../../assets/images/IconsDashboardLeftSide/Прибуток графік.png";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const leftCard = [
   {
@@ -9,17 +17,41 @@ const leftCard = [
     amountBottom: "128$",
     extraData: [
       {
-        icon: "",
+        icon: (
+          <p
+            className={css.numberIcon}
+            style={{ backgroundColor: "green", color: "var(--light-gray)" }}
+          >
+            20
+          </p>
+        ),
         text: "Vanilla",
         price: "1K $",
       },
       {
-        icon: "",
+        icon: (
+          <p
+            className={css.numberIcon}
+            style={{
+              backgroundColor: "var(  --status-new)",
+              color: "var(--bg-input)",
+            }}
+          >
+            20
+          </p>
+        ),
         text: "Standard",
         price: "2K $",
       },
       {
-        icon: "",
+        icon: (
+          <p
+            className={css.numberIcon}
+            style={{ backgroundColor: "var(--red)", color: "var(--bg-input)" }}
+          >
+            10
+          </p>
+        ),
         text: "PRO",
         price: "3K $",
       },
@@ -30,6 +62,23 @@ const leftCard = [
     amountOfMoney: "612 200 $",
     numberOfPeople: "",
     amountBottom: "62$",
+    extraData: [
+      {
+        icon: <BsClipboardData size={24} className={css.icon} />,
+        text: "Маркетинг",
+        price: "5.2K $",
+      },
+      {
+        icon: <BsFillPeopleFill size={24} className={css.icon} />,
+        text: "Персонал",
+        price: "4.3K $",
+      },
+      {
+        icon: <BsBriefcaseFill size={24} className={css.icon} />,
+        text: "Операційні",
+        price: "3K $",
+      },
+    ],
   },
   {
     title: "Прибуток",
@@ -42,12 +91,52 @@ const leftCard = [
     amountOfMoney: "5 200 $",
     numberOfPeople: "120",
     amountBottom: "42$",
+    extraData: [
+      {
+        icon: gmail,
+        numberOfPeople: "50",
+        price1: "40$",
+        price: "2K $",
+      },
+      {
+        icon: meta,
+        numberOfPeople: "50",
+        price1: "40$",
+        price: "2K $",
+      },
+      {
+        icon: partners,
+        numberOfPeople: "20",
+        price1: "60$",
+        price: "1.2K $",
+      },
+    ],
   },
   {
     title: "Нові клієнти",
     amountOfMoney: "7 500 $",
     numberOfPeople: "50",
     amountBottom: "150$",
+    extraData: [
+      {
+        icon: gmail,
+        numberOfPeople: "20",
+        price1: "100$",
+        price: "2K $",
+      },
+      {
+        icon: meta,
+        numberOfPeople: "20",
+        price1: "90$",
+        price: "2K $",
+      },
+      {
+        icon: partners,
+        numberOfPeople: "10",
+        price1: "60$",
+        price: "1.2K $",
+      },
+    ],
   },
 ];
 
@@ -65,13 +154,53 @@ export default function DashboardLeftSide() {
               </div>
               <p className={css.amountOfMoney}>{card.amountOfMoney}</p>
               <div className={css.bottomPart}>
-                <p className={css.numberOfPeople}>{card.numberOfPeople}</p>
-                <BsPersonFill className={css.icon} size={18} />
-                <p className={css.amountBottom}>{card.amountBottom}</p>
+                <div className={css.bottomLeftPart}>
+                  <p className={css.numberOfPeople}>{card.numberOfPeople}</p>
+                  <BsPersonFill className={css.icon} size={18} />
+                  <p className={css.amountBottom}>{card.amountBottom}</p>
+                </div>
+
+                <span className={css.statisticalData}>+2.4%</span>
               </div>
             </div>
 
-            <div className={css.lightCard}></div>
+            <div className={css.lightCard}>
+              {card.title !== "Прибуток" ? (
+                <ul className={css.lightCardList}>
+                  {card.extraData.map((data, index) => (
+                    <li key={index} className={css.lightCardItem}>
+                      {card.title === "Витрати" || card.title === "Продажі" ? (
+                        <>
+                          <div className={css.iconAndText}>
+                            {data.icon}
+                            <p className={css.dataText}>{data.text}</p>
+                          </div>
+                          <p className={css.dataPrice}>{data.price}</p>
+                        </>
+                      ) : (
+                        <>
+                          <div className={css.iconAndText}>
+                            <span className={css.imgBox}>
+                              <img src={data.icon} alt="" className={css.img} />
+                            </span>
+                            <p className={css.people}>{data.numberOfPeople}</p>
+                            <BsPersonFill className={css.icon} size={18} />
+                            <p className={css.money}>{data.price1}</p>
+                          </div>
+                          <p className={css.dataPrice}>{data.price}</p>
+                        </>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <img src={profit} alt="" />
+              )}
+            </div>
+
+            <div className={css.threeDotsBox}>
+              <BsThreeDotsVertical size={20} className={css.icon} />
+            </div>
           </li>
         ))}
       </ul>
